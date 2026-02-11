49790372d107489962ba2a687ad646f9d93024c5
IkhsanPramandika <mikhsanpra26@gmail.com>
2026-02-11 17:25:34 +0700

    feat: add GitHub Actions workflow for CI/CD

--- src/test/java/dev1/tests/base/BaseTest.java
+++ src/test/java/dev1/tests/base/BaseTest.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.Attachment;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
@@ -22,14 +23,23 @@ public class BaseTest {
     protected static String mainWindowHandle;
     protected static WebDriverWait wait;
 
-    @BeforeSuite
     public void setUp() throws IOException {
         config = new Properties();
         FileInputStream fis = new FileInputStream("src/test/resources/config.properties");
         config.load(fis);
 
+        // 1. Setup WebDriverManager
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+
+        // 2. Konfigurasi ChromeOptions untuk CI/CD (Headless)
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new"); //BISA DIMATIKAN
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 
